d1d342e78647a42a6aa460c7e22e66b9ff2ee5ab
griffi-gh <prasol258@gmail.com>
2026-07-14 17:08:31 +0200

    cudaPackages overlay: override cudatoolkit (legacy one)

--- nix/overlays/mkCudaOverlay.nix
+++ nix/overlays/mkCudaOverlay.nix
@@ -5,6 +5,10 @@ scaleScope: final: prev: {
       # the main one :3
       cuda_nvcc = scaleScope.scale-nvcc;
 
+      # legacy combined toolkit
+      # (hack: our scale-nvcc already merges in libs/includes from scale-runtime so this just works)
+      cudatoolkit = scaleScope.scale-nvcc;
+
       # (HACK: we have all of the libs in single scale-runtime derivation rn)
       cuda_cudart = scaleScope.scale-runtime;
       cuda_compat = scaleScope.scale-runtime;
@@ -16,7 +20,7 @@ scaleScope: final: prev: {
       libcusparse_lt = scaleScope.scale-runtime;
       cudnn = scaleScope.scale-runtime;
 
-      # override both legacy and the new alias
+      # cccl: override both legacy and the new alias
       cuda_cccl = scaleScope.cccl;
       cccl = scaleScope.cccl;
     }
