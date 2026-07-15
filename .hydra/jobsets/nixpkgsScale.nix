9897bfd4dd5abb494817414e5096c289f1fc5a7c
griffi-gh <prasol258@gmail.com>
2026-07-15 19:03:54 +0200

    Revert "uhm eval entirety of nixpkgs???? (scary but uhm atp just fuck it)"
    This reverts commit fc92dd46ca8a204c7918dec95e9eb375f0f48021.

--- .hydra/jobsets/nixpkgsScale.nix
+++ .hydra/jobsets/nixpkgsScale.nix
@@ -8,48 +8,25 @@
 let
   lib = import "${nixpkgs}/lib";
 
-  # THIS IS BORROWED/ADAPTED FROM
-  # https://github.com/nixos-cuda/hydra-jobsets/blob/master/jobsets/cuda-packages.nix
-  # (thanks ^w^)
-  ci = import "${nixpkgs}/ci" {
-    inherit system nixpkgs;
-  };
-  nixpkgsConfig = {
-    allowUnfree = true;
-    cudaSupport = true;
-    inHydra = true;
-    allowAliases = false;
-  };
-  evalCudaSupportFalse = ci.eval {
-    extraNixpkgsConfig = nixpkgsConfig // {
-      cudaSupport = false;
-    };
-  };
-  evalCudaSupportTrue = ci.eval {
-    extraNixpkgsConfig = nixpkgsConfig;
-  };
-  getAttrs =
-    dir:
-    let
-      raw = builtins.readFile "${dir}/${system}/paths.json";
-      data = builtins.unsafeDiscardStringContext raw;
-    in
-    builtins.fromJSON data;
-  before = getAttrs (evalCudaSupportFalse.baseline { evalSystems = [ system ]; });
-  after = getAttrs (evalCudaSupportTrue.baseline { evalSystems = [ system ]; });
-  isAddedOrChanged = name: !(before ? ${name}) || after.${name} != before.${name};
-  changed = lib.filter isAddedOrChanged (lib.attrNames after);
-  attrPaths = map (path: lib.init (lib.splitString "." path)) changed;
-
   flake = import src;
-  pkgSet = flake.legacyPackages.${system}."nixpkgsScale${suffix}";
-  jobs = lib.foldl' (
-    acc: path:
+  lp = flake.legacyPackages.${system};
+
+  # TODO: automate this
+  packages = [
+    "gromacs"
+    "colmap"
+    "opencv"
+    "blender"
+    "cudaPackages.saxpy"
+  ];
+
+  pkgSet = lp."nixpkgsScale${suffix}";
+  jobs = lib.genAttrs packages (
+    p:
     let
-      name = lib.concatStringsSep "." path;
-      value = lib.attrByPath path null pkgSet;
+      attrPath = lib.splitString "." p;
     in
-    if value == null then acc else acc // { ${name} = value; }
-  ) { } attrPaths;
+    lib.attrsets.attrByPath attrPath null pkgSet
+  );
 in
 jobs
