fc92dd46ca8a204c7918dec95e9eb375f0f48021
griffi-gh <prasol258@gmail.com>
2026-07-15 18:51:59 +0200

    uhm eval entirety of nixpkgs???? (scary but uhm atp just fuck it)

--- .hydra/jobsets/nixpkgsScale.nix
+++ .hydra/jobsets/nixpkgsScale.nix
@@ -8,25 +8,48 @@
 let
   lib = import "${nixpkgs}/lib";
 
-  flake = import src;
-  lp = flake.legacyPackages.${system};
-
-  # TODO: automate this
-  packages = [
-    "gromacs"
-    "colmap"
-    "opencv"
-    "blender"
-    "cudaPackages.saxpy"
-  ];
-
-  pkgSet = lp."nixpkgsScale${suffix}";
-  jobs = lib.genAttrs packages (
-    p:
+  # THIS IS BORROWED/ADAPTED FROM
+  # https://github.com/nixos-cuda/hydra-jobsets/blob/master/jobsets/cuda-packages.nix
+  # (thanks ^w^)
+  ci = import "${nixpkgs}/ci" {
+    inherit system nixpkgs;
+  };
+  nixpkgsConfig = {
+    allowUnfree = true;
+    cudaSupport = true;
+    inHydra = true;
+    allowAliases = false;
+  };
+  evalCudaSupportFalse = ci.eval {
+    extraNixpkgsConfig = nixpkgsConfig // {
+      cudaSupport = false;
+    };
+  };
+  evalCudaSupportTrue = ci.eval {
+    extraNixpkgsConfig = nixpkgsConfig;
+  };
+  getAttrs =
+    dir:
     let
-      attrPath = lib.splitString "." p;
+      raw = builtins.readFile "${dir}/${system}/paths.json";
+      data = builtins.unsafeDiscardStringContext raw;
     in
-    lib.attrsets.attrByPath attrPath null pkgSet
-  );
+    builtins.fromJSON data;
+  before = getAttrs (evalCudaSupportFalse.baseline { evalSystems = [ system ]; });
+  after = getAttrs (evalCudaSupportTrue.baseline { evalSystems = [ system ]; });
+  isAddedOrChanged = name: !(before ? ${name}) || after.${name} != before.${name};
+  changed = lib.filter isAddedOrChanged (lib.attrNames after);
+  attrPaths = map (path: lib.init (lib.splitString "." path)) changed;
+
+  flake = import src;
+  pkgSet = flake.legacyPackages.${system}."nixpkgsScale${suffix}";
+  jobs = lib.foldl' (
+    acc: path:
+    let
+      name = lib.concatStringsSep "." path;
+      value = lib.attrByPath path null pkgSet;
+    in
+    if value == null then acc else acc // { ${name} = value; }
+  ) { } attrPaths;
 in
 jobs
