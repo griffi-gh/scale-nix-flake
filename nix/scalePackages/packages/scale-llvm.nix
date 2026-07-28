a2d56101a085c57afeeb8c079ec21a88b1c23cbe
griffi-gh <prasol258@gmail.com>
2026-07-28 23:55:36 +0200

    fix interactive usage of nvcc on C code

--- nix/scalePackages/packages/scale-llvm.nix
+++ nix/scalePackages/packages/scale-llvm.nix
@@ -17,13 +17,20 @@ wrapCCWith {
 
   includeFortifyHeaders = false;
 
-  extraBuildCommands = ''
+  extraBuildCommands = let
+    preincludeHack = builtins.toFile "nix-preinclude.h" ''
+      // HACK: emulate leaky cstdlib header from CUDA
+      #if defined(__cplusplus)
+        #include <cstdlib>
+      #endif
+    '';
+  in ''
     # kill hardening
     > $out/nix-support/add-hardening.sh
 
-    # HACK: ensure redscale wrappers are searched before cstdlib
-    # HACK: emulate leaky cstdlib header from CUDA
+    # HACK: ensure redscale wrappers are searched before cstdlib;
+    # also, include our preinclude hack
     flags="$(< $out/nix-support/libcxx-cxxflags)"
-    echo "-isystem ${scale-runtime}/include -isystem ${scale-runtime}/include/redscale_impl/wrappers $flags -include cstdlib" > $out/nix-support/libcxx-cxxflags
+    echo "-isystem ${scale-runtime}/include -isystem ${scale-runtime}/include/redscale_impl/wrappers $flags -include ${preincludeHack}" > $out/nix-support/libcxx-cxxflags
   '';
 }
