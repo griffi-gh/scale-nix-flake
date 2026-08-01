836fe68778e28517943572c7f80afc1caf932adf
griffi-gh <prasol258@gmail.com>
2026-08-02 01:34:47 +0200

    override all CUDA versions

--- nix/overlays/mkCudaOverlay.nix
+++ nix/overlays/mkCudaOverlay.nix
@@ -1,30 +1,39 @@
-scaleScope: final: prev: {
-  # TODO overlays for other CUDA versions (e.g. cudaPackages_13 etc)
-  cudaPackages = prev.cudaPackages.overrideScope (
-    cf: cp: {
-      # the main one :3
-      cuda_nvcc = scaleScope.scale-nvcc;
+scaleScope: final: prev:
+let
+  inherit (final) lib;
 
-      # legacy combined toolkit
-      # (hack: our scale-nvcc already merges in libs/includes from scale-runtime so this just works)
-      cudatoolkit = scaleScope.scale-nvcc;
+  mkOverrideCudaScope =
+    cudaScope:
+    let
+      nvccPackage = scaleScope.scale-nvcc.override { cudaPackages = cudaScope; };
+    in
+    cudaScope.overrideScope (
+      cudaFinal: cudaPrev: {
+        # the main one :3
+        cuda_nvcc = nvccPackage;
+        # legacy combined toolkit
+        # (hack: scale-nvcc already merges libs/includes from scale-runtime, so this just works)
+        cudatoolkit = nvccPackage;
 
-      # (HACK: we have all of the libs in single scale-runtime derivation rn)
-      cuda_cudart = scaleScope.scale-runtime;
-      cuda_compat = scaleScope.scale-runtime;
-      cuda_nvrtc = scaleScope.scale-runtime;
-      libcufft = scaleScope.scale-runtime;
-      libcublas = scaleScope.scale-runtime;
-      libcurand = scaleScope.scale-runtime;
-      libcusolver = scaleScope.scale-runtime;
-      libcusparse = scaleScope.scale-runtime;
-      libcusparse_lt = scaleScope.scale-runtime;
-      libnvjitlink = scaleScope.scale-runtime;
-      cudnn = scaleScope.scale-runtime;
+        # HACK: all libs currently live in a single scale-runtime derivation.
+        cuda_cudart = scaleScope.scale-runtime;
+        cuda_compat = scaleScope.scale-runtime;
+        cuda_nvrtc = scaleScope.scale-runtime;
+        libcufft = scaleScope.scale-runtime;
+        libcublas = scaleScope.scale-runtime;
+        libcurand = scaleScope.scale-runtime;
+        libcusolver = scaleScope.scale-runtime;
+        libcusparse = scaleScope.scale-runtime;
+        libcusparse_lt = scaleScope.scale-runtime;
+        libnvjitlink = scaleScope.scale-runtime;
+        cudnn = scaleScope.scale-runtime;
 
-      # cccl: override both legacy and the new alias
-      cuda_cccl = scaleScope.cccl;
-      cccl = scaleScope.cccl;
-    }
-  );
-}
+        # cccl: override both the legacy name and the new alias
+        cuda_cccl = scaleScope.cccl;
+        cccl = scaleScope.cccl;
+      }
+    );
+in
+lib.mapAttrs (_: mkOverrideCudaScope) (
+  lib.filterAttrs (name: _: name == "cudaPackages" || lib.hasPrefix "cudaPackages_" name) prev
+)
